817e484ed0d593f31151907c653da5424c9c9d25
grayjay <grayjay@wordroute.com>
2014-12-03 00:24:41 -0500

    Organized functions.

--- src/Network/JsonRpc/Client.hs
+++ src/Network/JsonRpc/Client.hs
@@ -42,39 +42,21 @@ import Data.List (sortBy)
 import Control.Applicative (Applicative (..), Alternative (..), (<$>), (<*>), (<|>))
 import Control.Monad.Error (ErrorT (..), throwError, lift, (<=<))
 
--- | Relationship between the parameters ('ps'), return type ('r'),
---   and client-side function ('f') of a JSON-RPC method.
-class ClientFunction ps r f | ps r -> f where
-    toBatch :: Text -> A.Object -> ps -> ResultType r -> f
-
-instance A.FromJSON a => ClientFunction () a (Batch a) where
-    toBatch name args _ _ = Batch 1 [Request2 name False args] toResult
-        where toResult = decode <=< rsResult . head
-              decode rs = case A.fromJSON rs of
-                           A.Success x -> Right x
-                           A.Error msg -> Left $ rpcError clientCode $ pack $ "Client received wrong result type: " ++ msg
-
-instance (ClientFunction ps r f, A.ToJSON a) => ClientFunction (a ::: ps) r (a -> f) where
-    toBatch name args (p ::: ps) s a = toBatch name (H.insert p (A.toJSON a) args) ps s
-
 -- | Function used to send requests to the server.
 --   'Nothing' represents no response, as when a JSON-RPC
 --   server receives only notifications.
 type Server m = ByteString -> m (Maybe ByteString)
 
--- | Creates a function for calling a JSON-RPC method on the server.
-toFunction :: (Monad m, Functor m, ClientFunction ps r f, ComposeMultiParam (Batch r -> RpcResult m r) f g) =>
-              Server m       -- ^ Function for sending requests to the server.
-           -> Signature ps r -- ^ Method signature.
-           -> g              -- ^ Client-side function with a return type of @'RpcResult' m r@.
-toFunction server s = compose (runBatch server) (toBatchFunction s)
+type Result = Either RpcError
 
--- | Creates a function for calling a JSON-RPC method on the server as a notification.
-toFunction_ :: (Monad m, Functor m, ClientFunction ps r f, ComposeMultiParam (Batch r -> RpcResult m ()) f g) =>
-               Server m       -- ^ Function for sending requests to the server.
-            -> Signature ps r -- ^ Method signature.
-            -> g              -- ^ Client-side function with a return type of @'RpcResult' m ()@.
-toFunction_ server signature = compose (runBatch server . voidBatch) (toBatchFunction signature)
+-- | Signature specifying the name,
+--   parameter names and types ('ps'), and return type ('r') of a method.
+data Signature ps r = Signature Text ps
+
+-- | A node in a linked list specifying parameter names and types.
+--   It is right associative.
+data p ::: ps = Text ::: ps
+infixr :::
 
 -- | Creates a function for calling a JSON-RPC method as part of a batch request.
 toBatchFunction :: ClientFunction ps r f =>
@@ -86,7 +68,24 @@ toBatchFunction s@(Signature name ps) = toBatch name H.empty ps (resultType s)
 toBatchFunction_ :: (ClientFunction ps r f, ComposeMultiParam (Batch r -> Batch ()) f g) =>
                     Signature ps r -- ^ Method signature.
                  -> g              -- ^ Client-side function with a return type of @'Batch' ()@.
-toBatchFunction_ signature = compose voidBatch (toBatchFunction signature)
+toBatchFunction_ = composeWithBatch voidBatch
+
+-- | Creates a function for calling a JSON-RPC method on the server.
+toFunction :: (Monad m, Functor m, ClientFunction ps r f, ComposeMultiParam (Batch r -> RpcResult m r) f g) =>
+              Server m       -- ^ Function for sending requests to the server.
+           -> Signature ps r -- ^ Method signature.
+           -> g              -- ^ Client-side function with a return type of @'RpcResult' m r@.
+toFunction = composeWithBatch . runBatch
+
+-- | Creates a function for calling a JSON-RPC method on the server as a notification.
+toFunction_ :: (Monad m, Functor m, ClientFunction ps r f, ComposeMultiParam (Batch r -> RpcResult m ()) f g) =>
+               Server m       -- ^ Function for sending requests to the server.
+            -> Signature ps r -- ^ Method signature.
+            -> g              -- ^ Client-side function with a return type of @'RpcResult' m ()@.
+toFunction_ server = composeWithBatch $ runBatch server . voidBatch
+
+composeWithBatch :: (ClientFunction ps r g, ComposeMultiParam f g h) => f -> Signature ps r -> h
+composeWithBatch f = compose f . toBatchFunction
 
 -- | Evaluates a batch.  The process depends on its size:
 --   
@@ -97,8 +96,8 @@ toBatchFunction_ signature = compose voidBatch (toBatchFunction signature)
 -- 3. If the batch has multiple requests, they are sent as an array of request objects.
 runBatch :: (Monad m, Functor m) =>
             Server m      -- ^ Function for sending requests to the server.
-         -> Batch a       -- ^ Batch to be evaluated.
-         -> RpcResult m a -- ^ Result.
+         -> Batch r       -- ^ Batch to be evaluated.
+         -> RpcResult m r -- ^ Result.
 runBatch server batch = let addId rq2 i = Request (rq2Method rq2) idField (rq2Params rq2)
                                 where idField = if rq2IsNotification rq2 then Nothing else Just $ A.Number $ fromInteger i
                             requests = zipWith addId (bRequests batch) [1..]
@@ -115,14 +114,19 @@ runBatch server batch = let addId rq2 i = Request (rq2Method rq2) idField (rq2Pa
                           json <- sendToServer
                           ErrorT $ return $ bToResult batch (sort json)
 
-type Result = Either RpcError
+-- | Converts all requests in a batch to notifications.
+voidBatch :: Batch r -> Batch ()
+voidBatch batch = Batch { bNonNotifications = 0
+                        , bRequests = map toNotification $ bRequests batch
+                        , bToResult = const $ return () }
+    where toNotification rq2 = rq2 { rq2IsNotification = True }
 
 -- | A batch call.  Batch multiple requests by combining
 --   values of this type using its 'Applicative' and 'Alternative'
 --   instances before running them with 'runBatch'.
-data Batch a = Batch { bNonNotifications :: Int
+data Batch r = Batch { bNonNotifications :: Int
                      , bRequests :: [Request2]
-                     , bToResult :: [Response] -> Result a }
+                     , bToResult :: [Response] -> Result r }
 
 instance Functor Batch where
     fmap f (Batch n rqs g) = Batch n rqs ((f <$>) . g)
@@ -140,12 +144,29 @@ combine f (Batch n1 rqs1 g1) (Batch n2 rqs2 g2) = let g3 rs = g1 rs1 `f` g2 rs2
                                                           where (rs1, rs2) = splitAt n1 rs
                                                   in Batch (n1 + n2) (rqs1 ++ rqs2) g3
 
--- | Converts all requests in a batch to notifications.
-voidBatch :: Batch a -> Batch ()
-voidBatch batch = Batch { bNonNotifications = 0
-                        , bRequests = map toNotification $ bRequests batch
-                        , bToResult = const $ return () }
-    where toNotification rq2 = rq2 { rq2IsNotification = True }
+data ResultType r = ResultType
+
+resultType :: Signature ps r -> ResultType r
+resultType _ = ResultType
+
+-- | Code used for all client-side errors.  It is -31999.
+clientCode :: Int
+clientCode = -31999
+
+-- | Relationship between the parameters ('ps'), return type ('r'),
+--   and client-side function ('f') of a JSON-RPC method.
+class ClientFunction ps r f | ps r -> f where
+    toBatch :: Text -> A.Object -> ps -> ResultType r -> f
+
+instance A.FromJSON r => ClientFunction () r (Batch r) where
+    toBatch name args _ _ = Batch 1 [Request2 name False args] toResult
+        where toResult = decode <=< rsResult . head
+              decode rs = case A.fromJSON rs of
+                           A.Success x -> Right x
+                           A.Error msg -> Left $ rpcError clientCode $ pack $ "Client received wrong result type: " ++ msg
+
+instance (ClientFunction ps r f, A.ToJSON a) => ClientFunction (a ::: ps) r (a -> f) where
+    toBatch name args (p ::: ps) s a = toBatch name (H.insert p (A.toJSON a) args) ps s
 
 -- | Relationship between a function ('g') taking any number of arguments and yielding a @'Batch' a@,
 --   a function ('f') taking a @'Batch' a@, and the function ('h') formed by applying g to all of its
@@ -181,21 +202,3 @@ instance A.FromJSON Response where
                 \v -> Response <$>
                       (Right <$> v .: "result" <|> Left <$> v .: "error")  <*>
                       v .: "id"
-
-data ResultType r = ResultType
-
--- | Signature specifying the name,
---   parameter names and types ('ps'), and return type ('r') of a method.
-data Signature ps r = Signature Text ps
-
-resultType :: Signature ps r -> ResultType r
-resultType _ = ResultType
-
--- | A node in a linked list specifying parameter names and types.
---   It is right associative.
-data a ::: ps = Text ::: ps
-infixr :::
-
--- | Code used for all client-side errors.  It is -31999.
-clientCode :: Int
-clientCode = (-31999)
--- src/Network/JsonRpc/ServerAdapter.hs
+++ src/Network/JsonRpc/ServerAdapter.hs
@@ -8,24 +8,24 @@ module Network.JsonRpc.ServerAdapter ( -- * Server Methods
                                        toServerMethod
                                      , ConvertParams ) where
 
-import Network.JsonRpc.Server ( Method, MethodParams
-                              , Parameter (..), toMethod, (:+:) (..))
-import Network.JsonRpc.Client (Signature (..), (:::) (..))
-
--- | Relationship between the parameters in a 'Signature' ('p1')
---   and the parameters expected by 'toMethod' ('p2') for a given
---   RPC method.
-class ConvertParams p1 p2 | p1 -> p2 where
-    createServerParams :: p1 -> p2
-
-instance ConvertParams () () where
-    createServerParams = id
-
-instance ConvertParams p1 p2 => ConvertParams (a ::: p1) (a :+: p2) where
-    createServerParams (name ::: ps1) = Required name :+: createServerParams ps1
+import Network.JsonRpc.Client( Signature (..), (:::) (..))
+import Network.JsonRpc.Server( Method, MethodParams
+                             , Parameter (..), toMethod, (:+:) (..))
 
 -- | Creates a method from the given signature and function.
 --   The parameters of the resulting method match the order
 --   and types of the parameters in the signature and are all 'Required'.
-toServerMethod :: (ConvertParams p1 p2, MethodParams f p2 m r) => Signature p1 r -> f -> Method m
+toServerMethod :: (ConvertParams ps1 ps2, MethodParams f ps2 m r) => Signature ps1 r -> f -> Method m
 toServerMethod (Signature name ps) f = toMethod name f $ createServerParams ps
+
+-- | Relationship between the parameters in a 'Signature' ('ps1')
+--   and the parameters expected by 'toMethod' ('ps2') for a given
+--   RPC method.
+class ConvertParams ps1 ps2 | ps1 -> ps2 where
+    createServerParams :: ps1 -> ps2
+
+instance ConvertParams () () where
+    createServerParams = id
+
+instance ConvertParams ps1 ps2 => ConvertParams (p ::: ps1) (p :+: ps2) where
+    createServerParams (name ::: ps1) = Required name :+: createServerParams ps1
